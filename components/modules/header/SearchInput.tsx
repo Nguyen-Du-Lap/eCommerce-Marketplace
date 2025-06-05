db3c60fbdd184e781fbf90b676bec02806b580d1
Nguyen-Du-Lap <20130302@st.hcmuaf.edu.vn>
2025-06-05 10:52:31 +0700

    fix search input

--- components/modules/header/SearchInput.tsx
+++ components/modules/header/SearchInput.tsx
@@ -7,30 +7,45 @@ import { cn } from "@/lib/utils";
 import { Search } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { m } from "framer-motion";
 import axios from "axios";
-import { TypeProductModel } from "@/types/models";
+import { TypeProductModel } from "@/types";
+import { useDebounce } from "@/hooks/useDebounce";
 
 export default function SearchInput({ className }: { className?: string }) {
   const [openDropDow, setOpenDropDow] = useState(false);
   const [data, setData] =  useState<TypeProductModel[]>();
+  const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search);
+  const [loading, setLoading] = useState(false);
+
   const handleSearch = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const search = e.currentTarget.value;
-    if(search.length>1) {
-      console.log("searching for: ", search);
-      await axios
-      .get(process.env.NEXT_PUBLIC_API_URL + "/api/products", { 
-        params: { search } 
-      }).then((response) => {
-        console.log(response.data)
-        setData(response.data.data)
+    setSearch(search);
+  }
+
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+
+      const products = await axios
+      .get(process.env.NEXT_PUBLIC_API_URL + `/api/products/search/${search}?page=0&size=5&sort=name`)
+      .then((response) => {
+        console.log(response.data.result.content)
+        return response.data.result.content
       })
       .catch((err) => console.log(err))
       .finally(() => {});
 
+      setData(products);
+      setLoading(false);
+
     }
-  }
+
+    loadData();
+  }, [debouncedSearch]);
+
   return (
     <div className={cn(className, "px-2, relative")} onMouseLeave={() => setOpenDropDow(false)}>
       <Input
@@ -55,22 +70,23 @@ export default function SearchInput({ className }: { className?: string }) {
         )}
       >
         <div className="flex flex-col">
+          {loading && <div>Loading...</div>}
           {
-            data?.map((item: TypeProductModel, idx) => (
+            !loading && data?.map((item: TypeProductModel, idx) => (
             <Link
               key={idx}
               href="/"
               className="flex items-center gap-4 py-4 px-4 border-2 border-white hover:border-secondary-700"
             >
               <Image
-                src={item.images[0].url}
+                src={item.images[0].imageUrl}
                 width={80}
                 height={80}
                 alt="product image"
               />
               <div className="flex flex-col gap-1">
                 <h3 className="label-1">{item.name}</h3>
-                <Badge variant="soldOut">{item.discount}</Badge>
+                <Badge variant="soldOut">20%</Badge>
                 <h3 className="label-1 text-">{item.price}$</h3>
               </div>
             </Link>

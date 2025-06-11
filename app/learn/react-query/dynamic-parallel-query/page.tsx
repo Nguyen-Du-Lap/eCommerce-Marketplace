203c0456d3e5f4527337700d038d396e8a2e16f0
Nguyen-Du-Lap <20130302@st.hcmuaf.edu.vn>
2025-06-11 09:36:17 +0700

    parallel-query

---
+++ app/learn/react-query/dynamic-parallel-query/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useQueries } from '@tanstack/react-query';
+import axios from 'axios';
+import React from 'react';
+
+const fetchCategories = (id: string) => axios.get(`http://localhost:8080/api/categories/${id}`);
+
+export default function DynamicParallelQueryPage(){
+    const categoryIds = ["2ca2876b-313f-11f0-8e43-e2740ae364af", "2ca272db-313f-11f0-8e43-e2740ae364af"]
+    useQueries({
+        queries: categoryIds.map((id) => ({
+            queryKey: ['category', id],
+            queryFn: () => fetchCategories(id),
+            enabled: !!id, // Ensure the query is only run if id is truthy
+        }))
+    })
+    return (
+        <div>
+            <h1>Dynamic Parallel Query Example</h1>
+        </div>
+    );
+};
---
+++ app/learn/react-query/parallel-query/page.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { TypeCategoryModel, TypeProductModel } from '@/types';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import React from 'react';
+
+const fetchProducts = () => axios.get('http://localhost:8080/api/products?page=0&size=10&sort=name');
+const fetchCategories = () => axios.get('http://localhost:8080/api/categories?page=0&size=10&sort=name');
+
+export default function ParallelQueryPage(){
+    const { data: products, isLoading: isLoadingProducts, isError: isErrorProducts, error: errorProducts, isFetching: isFetchingProducts } = useQuery({
+        queryKey: ['products'],
+        queryFn: fetchProducts,
+        select: (data) => data.data.result.content,
+        enabled: false
+    })
+
+    const { data: categories, isLoading: isLoadingCategories, isError: isErrorCategories, error: errorCategories, isFetching: isFetchingCategories } = useQuery({
+        queryKey: ['categories'],
+        queryFn: fetchCategories,
+        select: (data) => data.data.result.content,
+    })
+
+    console.log('isLoadingProducts:', isLoadingProducts, 'isFetchingProducts:', isFetchingProducts, 'isFetchingCategories:', isFetchingCategories);
+
+    if (isLoadingProducts || isLoadingCategories) {
+        return <div>Loading...</div>;
+    }
+    if (isErrorProducts) {
+        return <div>Error loading products: {(errorProducts as Error).message}</div>;
+    }
+    if (isErrorCategories) {
+        return <div>Error loading categories: {(errorCategories as Error).message}</div>;
+    }
+    return (
+        <div>
+            <h1>Parallel Query Example</h1>
+            <div>
+                <h2>Products</h2>
+                <ul>
+                    {products?.map((product: TypeProductModel) => (
+                        <li key={product.id}>{product.name}</li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <h2>Categories</h2>
+                <ul>
+                    {categories?.map((category: TypeCategoryModel) => (
+                        <li key={category.id}>{category.name}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
